bb66becd5ed2e3dcd3a6a654b832f9120a566fb6
Blaise Boscaccy <blaise.boscaccy@gmail.com>
2016-11-08 12:54:33 -0600

    tests: Add core import tests

--- test/NamespaceTests.cpp
+++ test/NamespaceTests.cpp
@@ -10,8 +10,15 @@ TEST(TestNamespace, TestCreation)
 	auto names = world.namespace_list();
 	ASSERT_EQ(names.size(), 3);
 	ASSERT_EQ(world.get_namespace("foo")->world(), &world);
-	ASSERT_EQ(world.namespace_list().size(), 2);
+	ASSERT_EQ(world.namespace_list().size(), 3);
 	ASSERT_EQ(bar->namespace_list().size(), 1);
 	ASSERT_EQ(bar->full_name(), "bar");
 	ASSERT_EQ(baz->full_name(), "bar.baz");
+}
+
+TEST(TestNamespace, TestCoreTypes)
+{
+	xerxzema::World world;
+	auto foo = world.get_namespace("foo");
+	ASSERT_EQ(true, foo->is_type("unit"));
 }
