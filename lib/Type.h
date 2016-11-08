b88497fed375fb8f878a21339c6defb0a5499cdb
Blaise Boscaccy <blaise.boscaccy@gmail.com>
2016-11-08 12:57:13 -0600

    refactor: Remove store and typeprovider.

--- lib/Type.h
+++ lib/Type.h
@@ -11,13 +11,6 @@ public:
 	virtual std::string Name() = 0;
 };
 
-class TypeProvider
-{
-public:
-	virtual bool is_type(const std::string& name) = 0;
-	virtual Type* type(const std::string& name) = 0;
-};
-
 class Atom : public Type
 {
 public:
