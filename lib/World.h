80678ae46e636387828ef0bea0f72561bac36191
Blaise Boscaccy <blaise.boscaccy@gmail.com>
2016-11-08 15:49:42 -0600

    world: Create llvm context

--- lib/World.h
+++ lib/World.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <map>
 #include <memory>
+#include <llvm/IR/LLVMContext.h>
 #include "Namespace.h"
 
 namespace xerxzema
@@ -12,8 +13,10 @@ public:
 	World();
 	Namespace* get_namespace(const std::string& name);
 	std::vector<std::string> namespace_list() const;
+	inline llvm::LLVMContext& context() { return ctx; }
 private:
 	void create_core_namespace();
 	std::map<std::string, std::unique_ptr<Namespace>> namespaces;
+	llvm::LLVMContext ctx;
 };
 };
