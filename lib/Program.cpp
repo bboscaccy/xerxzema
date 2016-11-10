3ee035641dfbbc157c04435bd520adb3f0bc984f
Blaise Boscaccy <blaise.boscaccy@gmail.com>
2016-11-10 16:38:05 -0600

    program: Create function type.

--- lib/Program.cpp
+++ lib/Program.cpp
@@ -1,4 +1,5 @@
 #include "Program.h"
+#include "llvm/IR/IRBuilder.h"
 
 namespace xerxzema
 {
@@ -23,7 +24,28 @@ void Program::add_output(const std::string &name, xerxzema::Type *type)
 
 void Program::code_gen(llvm::Module *module, llvm::LLVMContext &context)
 {
+	std::vector<llvm::Type*> data_types;
+	data_types.push_back(llvm::Type::getInt64Ty(context)); //frame counter
+	data_types.push_back(llvm::Type::getInt1Ty(context)); //state value
+	data_types.push_back(llvm::Type::getInt64Ty(context)); //offset table
+	//inputs
+	//outputs
+	//locals
+	auto program_data = llvm::StructType::create(context, data_types, _name + "_data");
 
+	std::vector<llvm::Type*> arg_types;
+	arg_types.push_back(program_data->getPointerTo());
+
+	auto function_type = llvm::FunctionType::get(llvm::Type::getInt64Ty(context),
+												 arg_types, false);
+	auto fn = llvm::Function::Create(function_type,
+									 llvm::GlobalValue::LinkageTypes::ExternalLinkage,
+									 _name, module);
+
+	llvm::IRBuilder<> builder(context);
+	auto block = llvm::BasicBlock::Create(context, "entry", fn);
+	builder.SetInsertPoint(block);
+	builder.CreateRet(llvm::ConstantInt::get(llvm::Type::getInt64Ty(context), 0));
 }
 
 };
--- test/JitTests.cpp
+++ test/JitTests.cpp
@@ -6,5 +6,6 @@ TEST(TestJit, TestCreation)
 {
 	xerxzema::World world;
 	auto jit = world.create_jit();
+	world.get_namespace("core")->get_program("test");
 	jit->compile_namespace(world.get_namespace("core"));
 }
