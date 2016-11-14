9ce12605a53ffd2ca0f6be4d66590bff4b4d0ebc
Blaise Boscaccy <blaise.boscaccy@gmail.com>
2016-11-13 20:02:52 -0600

    jit tests

--- test/JitTests.cpp
+++ test/JitTests.cpp
@@ -9,10 +9,18 @@ TEST(TestJit, TestCreation)
 	auto jit = world.create_jit();
 	auto p = world.get_namespace("core")->get_program("test");
 	p->add_input("hi", world.get_namespace("core")->type("real"));
+
 	auto foo = p->reg("foo");
 	auto nop = std::make_unique<xerxzema::Instruction>();
 	nop->input(p->reg("head"));
 	nop->output(foo);
 	p->instruction(std::move(nop));
+
+	auto bar = p->reg("bar");
+	auto nop2 = std::make_unique<xerxzema::Instruction>();
+	nop2->input(foo);
+	nop2->output(bar);
+	p->instruction(std::move(nop2));
+
 	jit->compile_namespace(world.get_namespace("core"));
 }
