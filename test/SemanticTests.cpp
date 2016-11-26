cc66d9085d2786d6562368c76d6c3b51fede1880
Blaise Boscaccy <blaise.boscaccy@gmail.com>
2016-11-26 14:57:59 -0600

    tests: add semantic tests.

---
+++ test/SemanticTests.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include <sstream>
+#include <iostream>
+#include "../lib/Lexer.h"
+#include "../lib/Parser.h"
+#include "../lib/Semantic.h"
+#include "../lib/World.h"
+
+TEST(TestSemantic, TestValidateTop)
+{
+	std::stringstream ss;
+	ss << "prog foo(x:real) -> y:real\n" \
+		" x + x -> y; ";
+	xerxzema::Lexer lexer(ss);
+
+	xerxzema::World world;
+	auto ns = world.get_namespace("tests");
+
+	xerxzema::HandleTopLevelExpression top_level(ns);
+	auto expr = xerxzema::expression(lexer);
+
+	expr->accept(top_level);
+	ASSERT_TRUE(top_level.is_valid());
+}
