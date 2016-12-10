6587a4c84597d01e562a6a7f5451b0f16b80a8ba
Blaise Boscaccy <blaise.boscaccy@gmail.com>
2016-12-10 10:53:43 -0600

    rt: remove scheduler definitions for rt bindings.

--- lib/RT.cpp
+++ lib/RT.cpp
@@ -14,23 +14,3 @@ void xerxzema_print(const char* fmt, ...)
 	vprintf(fmt, args);
 	va_end(args);
 }
-
-xerxzema_scheduler xerxzema_create_scheduler()
-{
-	return new Scheduler();
-}
-
-void xerxzema_destroy_scheduler(xerxzema_scheduler sch)
-{
-	delete (Scheduler*)sch;
-}
-
-void xerxzema_run(xerxzema_scheduler scheduler, xerxzema_scheduler_fn startup, void* data)
-{
-	((Scheduler*)scheduler)->run();
-}
-
-void xerxzema_runone(xerxzema_scheduler scheduler, xerxzema_scheduler_fn startup, void* data)
-{
-	((Scheduler*)scheduler)->run();
-}
--- lib/RT.h
+++ lib/RT.h
@@ -5,19 +5,7 @@
 
 extern "C" {
 
-typedef void* xerxzema_scheduler;
-typedef void(*xerxzema_scheduler_fn)(void* data);
-
 void xerxzema_print(const char* fmt, ...);
-xerxzema_scheduler xerxzema_create_scheduler();
-void xerxzema_destroy_scheduler(xerxzema_scheduler);
-void xerxzema_runone(xerxzema_scheduler scheduler, xerxzema_scheduler_fn startup, void* data);
-void xerxzema_run(xerxzema_scheduler scheduler, xerxzema_scheduler_fn startup, void* data);
-void xerxzema_schedule_relative(xerxzema_scheduler scheduler, xerxzema_scheduler_fn fn,
-								uint64_t nanoseconds);
-void xerxzema_schedule_now(xerxzema_scheduler scheduler, xerxzema_scheduler_fn fn,
-						   uint64_t nanoseconds);
-
 };
 
 #endif
