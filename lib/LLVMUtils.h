04b03c94892eb0b3ae9eba2e807d70c4876ebe6c
Blaise Boscaccy <blaise.boscaccy@gmail.com>
2016-12-20 12:48:13 -0600

    utils: return Constant* type

--- lib/LLVMUtils.h
+++ lib/LLVMUtils.h
@@ -13,32 +13,32 @@ namespace xerxzema
 {
 
 template<int N>
-inline llvm::Value* const_int_value(llvm::LLVMContext& context, uint64_t v)
+inline llvm::Constant* const_int_value(llvm::LLVMContext& context, uint64_t v)
 {
 	return llvm::ConstantInt::get(context, llvm::APInt(N, v));
 }
 
-inline llvm::Value* const_int1(llvm::LLVMContext& context, uint64_t v)
+inline llvm::Constant* const_int1(llvm::LLVMContext& context, uint64_t v)
 {
 	return llvm::ConstantInt::get(llvm::Type::getInt1Ty(context), v);
 }
 
-inline llvm::Value* const_int8(llvm::LLVMContext& context, uint64_t v)
+inline llvm::Constant* const_int8(llvm::LLVMContext& context, uint64_t v)
 {
 	return llvm::ConstantInt::get(llvm::Type::getInt8Ty(context), v);
 }
 
-inline llvm::Value* const_int16(llvm::LLVMContext& context, uint64_t v)
+inline llvm::Constant* const_int16(llvm::LLVMContext& context, uint64_t v)
 {
 	return llvm::ConstantInt::get(llvm::Type::getInt16Ty(context), v);
 }
 
-inline llvm::Value* const_int32(llvm::LLVMContext& context, uint64_t v)
+inline llvm::Constant* const_int32(llvm::LLVMContext& context, uint64_t v)
 {
 	return llvm::ConstantInt::get(llvm::Type::getInt32Ty(context), v);
 }
 
-inline llvm::Value* const_int64(llvm::LLVMContext& context, uint64_t v)
+inline llvm::Constant* const_int64(llvm::LLVMContext& context, uint64_t v)
 {
 	return llvm::ConstantInt::get(llvm::Type::getInt64Ty(context), v);
 }
